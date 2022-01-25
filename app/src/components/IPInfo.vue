784da0879466517f034c8bc407a012d967430164
berkinduz <duzberkin@gmail.com>
2022-01-25 21:53:18 +0300

    Information box component created and designed

---
+++ app/src/components/IPInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="
+      absolute
+      -bottom-64
+      lg:-bottom-20
+      flex flex-col
+      lg:flex-row
+      bg-white
+      gap-y-4 gap-x-12
+      px-8
+      pt-8
+      pb-12
+      rounded-md
+    "
+  >
+    <!-- Flex Item #1 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+      <span class="text-2xl">8.8.8.8</span>
+    </div>
+    <!-- Flex Item #2 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Location</h3>
+      <span class="text-2xl">Izmir</span>
+    </div>
+    <!-- Flex Item #3 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+      <span class="text-2xl">UTC+3 07:00</span>
+    </div>
+    <!-- Flex Item #4 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">ISP</h3>
+      <span class="text-2xl">Google</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
