76ed38e4a8459d49686f4f13a0f63c1ebe258b27
berkinduz <duzberkin@gmail.com>
2022-01-25 23:43:10 +0300

    geological api imported and project relations completed

--- app/src/components/IPInfo.vue
+++ app/src/components/IPInfo.vue
@@ -17,26 +17,28 @@
     <!-- Flex Item #1 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">IP Address</h3>
-      <span class="text-2xl">8.8.8.8</span>
+      <span class="text-2xl">{{ ipInfo.address }}</span>
     </div>
     <!-- Flex Item #2 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Location</h3>
-      <span class="text-2xl">Izmir</span>
+      <span class="text-2xl">{{ ipInfo.state }}</span>
     </div>
     <!-- Flex Item #3 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">Timezone</h3>
-      <span class="text-2xl">UTC+3 07:00</span>
+      <span class="text-2xl">UTC {{ ipInfo.timezone }}</span>
     </div>
     <!-- Flex Item #4 -->
     <div class="flex flex-col">
       <h3 class="text-xs mb-2 uppercase">ISP</h3>
-      <span class="text-2xl">Google</span>
+      <span class="text-2xl">{{ ipInfo.isp }}</span>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["ipInfo"],
+};
 </script>
